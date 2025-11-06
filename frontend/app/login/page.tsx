0a919f2fe1c56e3ab5ad32bba81965288f23a830
Dev.Guh <guh.costa027@gmail.com>
2025-11-06 15:23:39 -0300

    Sobe

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -14,7 +14,7 @@ export default function Login() {
     setLoading(true);
 
     try {
-      const response = await fetch("http://localhost:3003/appBarber/login", {
+      const response = await fetch("https://appbarber-backend.onrender.com/appBarber/findListServices", {
         method: "POST",
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ loginEmail: email, loginSenha: password })
