193d9e826ca47c9bdc57e76efb37cd50584f69b2
Dev.Guh <guh.costa027@gmail.com>
2025-11-06 14:30:36 -0300

    Atualizando Rota

--- backEnd/src/app.ts
+++ backEnd/src/app.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv"
 import router from "./router/router.js"; 
-
+import type{Request,Response} from "express"
 import cors from "cors"
 const app = express();
 app.use(express.json())
@@ -11,7 +11,11 @@ dotenv.config();
 const port = 3003;
 
 app.use("/appBarber", router)
+app.get("/", (_req: Request, res: Response) => {
+  res.send("Backend online 🚀");
+});
 
 app.listen(port, () =>{
+    
     console.log(`Servidor Aberto ! ${port}`)
 })
