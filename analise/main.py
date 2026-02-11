c3a9f3b108e9784a11b69ba3cce408ebd159ce19
Dev.Guh <guh.costa027@gmail.com>
2026-02-11 19:12:13 -0300

    Vamo la

--- analise/main.py
+++ analise/main.py
@@ -11,6 +11,7 @@ app.add_middleware(
     allow_origins=["http://localhost:3000",
                    "https://app-barber-three.vercel.app"],  
     allow_credentials=True,
+    allow_origin_regex=r"https://.*\.vercel\.app",
     allow_methods=["*"],
     allow_headers=["*"],
 )
